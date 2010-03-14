1aa3dda0fb6bacc021b7c5758c108bd9b74281e6
Matt Wood <doowttam@gmail.com>
2010-03-13 19:13:02 -0600

    Completely switched up authentication

--- js/punchclock.js
+++ js/punchclock.js
@@ -1,44 +1,11 @@
-// Your basecamp url
-var baseUrl = '';
-var userId;
-
-function init() {}
-
-function login() {
-    // Display loading graphic
-    $('.loading').fadeIn();
-
-    $.ajaxSetup({
-        username: $('#basecamp_username').val(),
-        password: $('#basecamp_password').val(),
-        contentType: 'application/xml',
-        dataType: 'xml',
-        error: function(xhr, status, err) {
-            // Right now this condition will never fire
-            // because Air intercepts the any 401s
-            air.trace('There was an error: ' + xhr);
-            air.Introspector.Console.log(xhr);
-            if (xhr.status == 401) {
-                $('#messages').text('There was a problem with your Basecamp credentials.');
-                air.trace('Bad credentials');
-            }
-            $('.loading').fadeOut();
-        } 
-    });
-
-    // Just a simple request to check the users credentials and get their id
-    $.get(baseUrl + '/me.xml', function(data) {
-        userId = $(data).find('person > id').text();
-        $('.loading').fadeOut();
-        $('#login').css('display', 'none');
-        $('#mainWindow').css('display', 'block');
-        $('.loading').fadeIn();
-        getTodayReport();
-        getWeekReport();
-    });
+function init() {
+    window.htmlLoader.authenticate = false;  
+    checkSettings();
 }
 
 function getTodayReport() {
+    
+    air.trace('getTodayReport');
 
     var now_string = date_to_string(new Date());
     var url = baseUrl + '/time_entries/report.xml?subject_id=' + userId + '&from=' + now_string + 'to=' + now_string;
@@ -82,4 +49,76 @@ function date_to_string (date) {
         date = '0' + date;
     
     return year + month + date;
+}
+
+function checkSettings() {
+    var api_token = air.EncryptedLocalStore.getItem("api_token");
+    var basecamp_url = air.EncryptedLocalStore.getItem("basecamp_url");
+
+    if ( api_token === undefined || basecamp_url === undefined )
+        $('.settings').css('display', 'block');
+}
+
+function verifyAndSaveSettings() {
+    air.trace('verifyAndSaveSettings');
+
+    var api_token = $('#api_token').val();
+    var basecamp_url = 'https://' + $('#basecamp_url').val() + '.basecamphq.com';
+
+    var encodedPass = Base64.encode( api_token + ":X" );
+
+    $.ajax({
+        url: basecamp_url + '/me.xml',
+        contentType: 'application/xml',
+        dataType: 'xml',
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("Authorization", "Basic " + encodedPass);
+        },
+        success: function(data) {
+
+            air.trace('Success: Storing user data in store.');
+            var bytes = new air.ByteArray(); 
+            bytes.writeUTFBytes(api_token); 
+            air.EncryptedLocalStore.setItem("api_token", bytes);
+
+            bytes.writeUTFBytes(basecamp_url); 
+            air.EncryptedLocalStore.setItem("basecamp_url", bytes);
+
+            var userId = $(data).find('person > id').text();
+            bytes.writeUTFBytes(userId); 
+            air.EncryptedLocalStore.setItem("basecamp_user_id", bytes);
+
+        },
+        error: function(xhr, status, err) {
+
+            air.trace('Error');
+            air.Introspector.Console.log(xhr);
+            if (xhr.status == 401) {
+                air.trace('Bad credentials');
+            }
+
+        }
+    });
+
+    // $.ajaxSetup({
+    //     username: api_token,
+    //     password: 'X',
+    //     contentType: 'application/xml',
+    //     dataType: 'xml',
+    //     error: function(xhr, status, err) {
+    //         air.trace('There was an error: ' + xhr);
+    //         air.Introspector.Console.log(xhr);
+    //         if (xhr.status == 401) {
+    //             $('#messages').text('There was a problem with your Basecamp credentials.');
+    //             air.trace('Bad credentials');
+    //         }
+    //     } 
+    // });
+
+    // Just a simple request to check the users credentials and get their id
+    // $.get(, function(data) {
+    //     air.race('Response: ' + data);
+    //     userId = $(data).find('person > id').text();
+    //     air.trace('success!');
+    // });
 }
