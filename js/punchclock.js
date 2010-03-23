a188e43dac94ef0b6abba2969bbb2cce396aea15
Matt Wood <doowttam@gmail.com>
2010-03-22 22:06:22 -0500

    Fixes a really dumb bug where the report paramter strings weren't being generated correctly.

--- js/punchclock.js
+++ js/punchclock.js
@@ -103,7 +103,7 @@ function getTimeReports() {
 function getReport(report_id, report_param_string) {
     air.trace('getReport: ' + report_param_string);
     
-    $.get(base_url + '/time_entries/report.xml?' + report_param_string, function(data) {
+    $.get(base_url + '/time_entries/report.xml' + report_param_string, function(data) {
         var total = 0;
         $(data).find('time-entry > hours').each(function() {
             total += parseFloat($(this).text());
@@ -117,7 +117,7 @@ function getTodayReport() {
     air.trace('getTodayReport');
 
     var now_string = date_to_string(new Date());
-    var report_param_string = 'subject_id=' + user_id + '&from=' + now_string + 'to=' + now_string;
+    var report_param_string = '?subject_id=' + user_id + '&from=' + now_string + '&to=' + now_string;
 
     getReport('time_logged_today', report_param_string);
 }
@@ -128,7 +128,7 @@ function getWeekReport() {
     var now_string = date_to_string(date);
     date.setDate(date.getDate() - date.getDay());
     var start_string = date_to_string(date);
-    var report_param_string = 'subject_id=' + user_id + '&from=' + start_string + 'to=' + now_string;
+    var report_param_string = '?subject_id=' + user_id + '&from=' + start_string + '&to=' + now_string;
 
     getReport('time_logged_this_week', report_param_string);
 }
