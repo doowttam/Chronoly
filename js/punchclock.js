e0e639a71129c7fab7c940912109547bb3de5c2c
Matt Wood <doowttam@gmail.com>
2010-04-12 21:29:58 -0500

    Moved the timer code into its own file

--- js/punchclock.js
+++ js/punchclock.js
@@ -2,10 +2,6 @@ var api_token = '';
 var basecamp_url = '';
 var base_url = '';
 var user_id;
-var timerId;
-var timeSpent = 0;
-var totalTimeSpent = 0;
-var timerRunning = 0;
 
 function init() {
     air.trace('init');
@@ -301,40 +297,4 @@ function submitTime() {
             getTimeReports();
         }
     });
-}
-
-function toggleTimer() {
-    air.trace('toggleTimer');
-    if (timerRunning == 1) {
-        pauseTimer();
-    } else {
-        startTimer();
-    }
-}
-
-function startTimer() {
-    $('#startTimer').text('Pause Timer');
-    timerRunning = 1;
-    var startTimeStamp = new Date().valueOf();
-    timerId = setInterval( function() {
-        var diff = ( new Date().valueOf() - startTimeStamp ) / 3600000;
-        timeSpent = diff.toFixed(1);
-        $('#time_input').val(timeSpent + totalTimeSpent);
-    }, 360000 );
-    $('#timer_running').css('display', 'inline');
-}
-
-function pauseTimer() {
-    timerRunning = 0;
-    clearInterval(timerId);
-    totalTimeSpent = timeSpent + totalTimeSpent;
-    $('#startTimer').text('Start Timer');
-    $('#timer_running').css('display', 'none');
-}
-
-function resetTimer() {
-    clearInterval(timerId);
-    $('#startTimer').text('Start Timer');
-    $('#timer_running').css('display', 'none');
-    totalTimeSpent = 0;
 }
---
+++ js/timer.js
@@ -0,0 +1,39 @@
+var timerId;
+var timeSpent = 0;
+var totalTimeSpent = 0;
+var timerRunning = 0;
+
+function toggleTimer() {
+    if (timerRunning == 1) {
+        pauseTimer();
+    } else {
+        startTimer();
+    }
+}
+
+function startTimer() {
+    $('#startTimer').text('Pause Timer');
+    timerRunning = 1;
+    var startTimeStamp = new Date().valueOf();
+    timerId = setInterval( function() {
+        var diff = ( new Date().valueOf() - startTimeStamp ) / 3600000;
+        timeSpent = diff.toFixed(1);
+        $('#time_input').val(timeSpent + totalTimeSpent);
+    }, 360000 );
+    $('#timer_running').css('display', 'inline');
+}
+
+function pauseTimer() {
+    timerRunning = 0;
+    clearInterval(timerId);
+    totalTimeSpent = timeSpent + totalTimeSpent;
+    $('#startTimer').text('Start Timer');
+    $('#timer_running').css('display', 'none');
+}
+
+function resetTimer() {
+    clearInterval(timerId);
+    $('#startTimer').text('Start Timer');
+    $('#timer_running').css('display', 'none');
+    totalTimeSpent = 0;
+}
