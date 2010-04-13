f638c2aad16accf6f99f3b8ac4460a059633f0fc
Matt Wood <doowttam@gmail.com>
2010-04-12 21:21:42 -0500

    Added ability to pause timer

--- js/punchclock.js
+++ js/punchclock.js
@@ -4,6 +4,8 @@ var base_url = '';
 var user_id;
 var timerId;
 var timeSpent = 0;
+var totalTimeSpent = 0;
+var timerRunning = 0;
 
 function init() {
     air.trace('init');
@@ -301,17 +303,38 @@ function submitTime() {
     });
 }
 
+function toggleTimer() {
+    air.trace('toggleTimer');
+    if (timerRunning == 1) {
+        pauseTimer();
+    } else {
+        startTimer();
+    }
+}
+
 function startTimer() {
+    $('#startTimer').text('Pause Timer');
+    timerRunning = 1;
     var startTimeStamp = new Date().valueOf();
     timerId = setInterval( function() {
         var diff = ( new Date().valueOf() - startTimeStamp ) / 3600000;
         timeSpent = diff.toFixed(1);
-        $('#time_input').val(timeSpent);
+        $('#time_input').val(timeSpent + totalTimeSpent);
     }, 360000 );
     $('#timer_running').css('display', 'inline');
 }
 
+function pauseTimer() {
+    timerRunning = 0;
+    clearInterval(timerId);
+    totalTimeSpent = timeSpent + totalTimeSpent;
+    $('#startTimer').text('Start Timer');
+    $('#timer_running').css('display', 'none');
+}
+
 function resetTimer() {
     clearInterval(timerId);
+    $('#startTimer').text('Start Timer');
     $('#timer_running').css('display', 'none');
+    totalTimeSpent = 0;
 }
